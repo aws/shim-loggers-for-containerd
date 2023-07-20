6f20f6f270688c4af5ab02288afd123dda2b28ba
Ziwen Ning <ningziwe@amazon.com>
2023-07-20 09:26:17 -0700

    chore: update the file headers to SPDX style
    Signed-off-by: Ziwen Ning <ningziwe@amazon.com>

--- debug/debug_windows.go
+++ debug/debug_windows.go
@@ -1,17 +1,8 @@
+//go:build windows
 // +build windows
 
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License"). You may
-// not use this file except in compliance with the License. A copy of the
-// License is located at
-//
-//	http://aws.amazon.com/apache2.0/
-//
-// or in the "license" file accompanying this file. This file is distributed
-// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
-// express or implied. See the License for the specific language governing
-// permissions and limitations under the License.
+// SPDX-License-Identifier: Apache-2.0
 
 package debug
 
@@ -26,11 +17,11 @@ import (
 
 const (
 	// Maximum file size in MB
-	MAX_FILE_SIZE           float64 = 10
+	MAX_FILE_SIZE float64 = 10
 	// Maximum number of files generated in log rotation
-	MAX_ROLLS               int     = 24
+	MAX_ROLLS int = 24
 	// Messages will be in the format "time=... level=... msg=..."
-	SEE_LOG_CONFIG_TEMPLATE string  = `
+	SEE_LOG_CONFIG_TEMPLATE string = `
 <seelog type="asyncloop">
 	<outputs formatid="main">
 		<rollingfile filename="%s" type="size"
@@ -45,9 +36,9 @@ const (
 var (
 	envProgramData string = defaultIfBlank(os.Getenv("ProgramData"), `C:\ProgramData`)
 	// logFileDir is the default path for logs when log-file-dir option is not set
-	logFileDir     string = filepath.Join(envProgramData, "Amazon/ECS/log/shim-logger")
-	containerName  string
-	fileLogger     seelog.LoggerInterface
+	logFileDir    string = filepath.Join(envProgramData, "Amazon/ECS/log/shim-logger")
+	containerName string
+	fileLogger    seelog.LoggerInterface
 )
 
 func FlushLog() {
