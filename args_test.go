5027587d03996cd262cf9ff9f9d80509b75e8f3e
Ziwen Ning <ningziwe@amazon.com>
2023-07-20 11:24:16 -0700

    chore: update the file headers to SPDX style (#56)
    update the file headers to SPDX style
    
    
    By submitting this pull request, I confirm that you can use, modify,
    copy, and redistribute this contribution, under the terms of your
    choice.
    
    Signed-off-by: Ziwen Ning <ningziwe@amazon.com>

--- args_test.go
+++ args_test.go
@@ -1,15 +1,5 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License"). You may
-// not use this file except in compliance with the License. A copy of the
-// License is located at
-//
-//	http://aws.amazon.com/apache2.0/
-//
-// or in the "license" file accompanying this file. This file is distributed
-// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
-// express or implied. See the License for the specific language governing
-// permissions and limitations under the License.
+// SPDX-License-Identifier: Apache-2.0
 
 //go:build unit
 // +build unit
@@ -84,7 +74,9 @@ func testGetGlobalArgsNoError(t *testing.T) {
 // getGlobalArgs function with returned errors when required flag values are
 // missed. Since the logic in getGlobalArgs is examining required flag values
 // one by one in this order:
-//     container-id, container-name, log-driver,
+//
+//	container-id, container-name, log-driver,
+//
 // in this test, we will set flag one by one in the previous order to examine if
 // each value check worked as expected.
 func testGetGlobalArgsWithError(t *testing.T) {
