1e307068b3a5f32db346e148962d453fd08bc950
Ziwen Ning <ningziwe@amazon.com>
2023-09-18 18:15:35 -0700

    ci: add fluentd basic e2e tests (#71)
    - breaking out the 'log generation' piece out of the aws log test file
    into the main test file as func sendTestLogByContainerd so that it can
    be reused for every driver (which seems like a good idea)
    - add a fluentd test that does validation
    - adds fluentd to the GitHub action (note this launches a docker
    containerd with the fluentd server)
    - adds fluentd test to makefile
    
    Signed-off-by: Ziwen Ning <ningziwe@amazon.com>

--- e2e/awslogs_test.go
+++ e2e/awslogs_test.go
@@ -5,15 +5,11 @@ package e2e
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/cloudwatchlogs"
-	"github.com/containerd/containerd"
 	"github.com/containerd/containerd/cio"
-	"github.com/containerd/containerd/namespaces"
-	"github.com/containerd/containerd/oci"
 	"github.com/onsi/ginkgo/v2"
 	"github.com/onsi/gomega"
 )
@@ -28,31 +24,22 @@ const (
 	testAwslogsRegion             = "us-west-2"
 	testAwsLogsStream             = "test-stream"
 	testAwsLogsGroup              = "test-shim-logger"
-	testAwsLogsMessage            = "test-e2e-log"
 )
 
 var testAwslogs = func() {
 	// These tests are run in serial because we only define one log driver instance.
-	ginkgo.Describe("awslogs shim logger", ginkgo.Serial, func() { //nolint:typecheck
+	ginkgo.Describe("awslogs shim logger", ginkgo.Serial, func() {
 		var cwClient *cloudwatchlogs.Client
 		ginkgo.BeforeEach(func() {
 			cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(testAwslogsRegion))
 			gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
 			cwClient = cloudwatchlogs.NewFromConfig(cfg)
-			_, err = cwClient.DeleteLogStream(context.TODO(), &cloudwatchlogs.DeleteLogStreamInput{
-				LogStreamName: aws.String(testAwsLogsStream),
-				LogGroupName:  aws.String(testAwsLogsGroup),
-			})
-			gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
+			cleanupAwslogs(cwClient, testAwsLogsGroup, testAwsLogsStream)
 		})
 		ginkgo.AfterEach(func() {
-			_, err := cwClient.DeleteLogStream(context.TODO(), &cloudwatchlogs.DeleteLogStreamInput{
-				LogStreamName: aws.String(testAwsLogsStream),
-				LogGroupName:  aws.String(testAwsLogsGroup),
-			})
-			gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
+			cleanupAwslogs(cwClient, testAwsLogsGroup, testAwsLogsStream)
 		})
-		ginkgo.It("should send logs to awslogs log driver", func() { //nolint:typecheck
+		ginkgo.It("should send logs to awslogs log driver", func() {
 			args := map[string]string{
 				logDriverTypeKey:              awslogsDriverName,
 				containerIdKey:                testContainerId,
@@ -63,45 +50,26 @@ var testAwslogs = func() {
 				awslogsStreamKey:              testAwsLogsStream,
 			}
 			creator := cio.BinaryIO(*Binary, args)
-			// Create a new client connected to the containerd daemon
-			client, err := containerd.New(containerdAddress)
-			gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
-			defer client.Close()
-			// Create a new context with a customized namespace
-			ctx := namespaces.WithNamespace(context.Background(), "testAwslogs")
-			// Pull an image
-			image, err := client.Pull(ctx, testImage, containerd.WithPullUnpack)
-			gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
-			// Create a new container with the pulled image
-			container, err := client.NewContainer(ctx, testContainerId, containerd.WithImage(image),
-				containerd.WithNewSnapshot("test-snapshot", image), containerd.WithNewSpec(oci.WithImageConfig(image),
-					oci.WithProcessArgs("/bin/sh", "-c", fmt.Sprintf("echo '%s'", testAwsLogsMessage))))
-			gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
-			defer container.Delete(ctx, containerd.WithSnapshotCleanup) //nolint:errcheck // testing only
-			// Create a new task from the container and start it
-			task, err := container.NewTask(ctx, creator)
-			gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
-			defer task.Delete(ctx) //nolint:errcheck // testing only
-
-			err = task.Start(ctx)
-			gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
-
-			statusC, err := task.Wait(ctx)
-			gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
-			// Waiting for the task to finish
-			status := <-statusC
-			code, _, err := status.Result()
-			gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
-			gomega.Expect(code).Should(gomega.Equal(uint32(0)))
-
-			// Validating in AWS logs
-			cwOutput, err := cwClient.GetLogEvents(context.TODO(), &cloudwatchlogs.GetLogEventsInput{
-				LogStreamName: aws.String(testAwsLogsStream),
-				LogGroupName:  aws.String(testAwsLogsGroup),
-				Limit:         aws.Int32(1),
-			})
-			gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
-			gomega.Expect(*cwOutput.Events[0].Message).Should(gomega.Equal(testAwsLogsMessage))
+			sendTestLogByContainerd(creator, testLog)
+			validateTestLogsInAwslogs(cwClient, testAwsLogsGroup, testAwsLogsStream, testLog)
 		})
 	})
+}
+
+func validateTestLogsInAwslogs(client *cloudwatchlogs.Client, logGroupName string, logStreamName string, testLog string) {
+	cwOutput, err := client.GetLogEvents(context.TODO(), &cloudwatchlogs.GetLogEventsInput{
+		LogStreamName: aws.String(logGroupName),
+		LogGroupName:  aws.String(logStreamName),
+		Limit:         aws.Int32(1),
+	})
+	gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
+	gomega.Expect(*cwOutput.Events[0].Message).Should(gomega.Equal(testLog))
+}
+
+func cleanupAwslogs(client *cloudwatchlogs.Client, logGroupName string, logStreamName string) {
+	_, err := client.DeleteLogStream(context.TODO(), &cloudwatchlogs.DeleteLogStreamInput{
+		LogStreamName: aws.String(logGroupName),
+		LogGroupName:  aws.String(logStreamName),
+	})
+	gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
 }
--- e2e/main_test.go
+++ e2e/main_test.go
@@ -4,9 +4,15 @@
 package e2e
 
 import (
+	"context"
 	"flag"
+	"fmt"
 	"testing"
 
+	"github.com/containerd/containerd"
+	"github.com/containerd/containerd/cio"
+	"github.com/containerd/containerd/namespaces"
+	"github.com/containerd/containerd/oci"
 	"github.com/onsi/ginkgo/v2"
 	"github.com/onsi/gomega"
 )
@@ -15,12 +21,14 @@ const (
 	// LogDriver options
 	logDriverTypeKey  = "--log-driver"
 	awslogsDriverName = "awslogs"
+	fluentdDriverName = "fluentd"
 	containerIdKey    = "--container-id"
 	containerNameKey  = "--container-name"
 	testContainerId   = "test-container-id"
 	testContainerName = "test-container-name"
 	containerdAddress = "/run/containerd/containerd.sock"
 	testImage         = "public.ecr.aws/docker/library/ubuntu:latest"
+	testLog           = "test-e2e-log"
 )
 
 var (
@@ -36,8 +44,44 @@ func TestShimLoggers(t *testing.T) {
 		if *LogDriver == awslogsDriverName || *LogDriver == "" {
 			testAwslogs()
 		}
+		if *LogDriver == fluentdDriverName || *LogDriver == "" {
+			testFluentd()
+		}
 	})
 
 	gomega.RegisterFailHandler(ginkgo.Fail)
 	ginkgo.RunSpecs(t, description)
+}
+
+func sendTestLogByContainerd(creator cio.Creator, testLog string) {
+	// Create a new client connected to the containerd daemon
+	client, err := containerd.New(containerdAddress)
+	gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
+	defer client.Close()
+	// Create a new context with a customized namespace
+	ctx := namespaces.WithNamespace(context.Background(), "testShimLoggers")
+	// Pull an image
+	image, err := client.Pull(ctx, testImage, containerd.WithPullUnpack)
+	gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
+	// Create a new container with the pulled image
+	container, err := client.NewContainer(ctx, testContainerId, containerd.WithImage(image),
+		containerd.WithNewSnapshot("test-snapshot", image), containerd.WithNewSpec(oci.WithImageConfig(image),
+			oci.WithProcessArgs("/bin/sh", "-c", fmt.Sprintf("echo '%s'", testLog))))
+	gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
+	defer container.Delete(ctx, containerd.WithSnapshotCleanup) //nolint:errcheck // testing only
+	// Create a new task from the container and start it
+	task, err := container.NewTask(ctx, creator)
+	gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
+	defer task.Delete(ctx) //nolint:errcheck // testing only
+
+	err = task.Start(ctx)
+	gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
+
+	statusC, err := task.Wait(ctx)
+	gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
+	// Waiting for the task to finish
+	status := <-statusC
+	code, _, err := status.Result()
+	gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
+	gomega.Expect(code).Should(gomega.Equal(uint32(0)))
 }
