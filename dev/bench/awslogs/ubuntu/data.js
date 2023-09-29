733ffeb5115d9ab87338eba16c028585f5746de2
github-action-benchmark <github@users.noreply.github.com>
2023-09-29 22:19:34 +0000

    add Benchmark for awslogs (go) benchmark result for d03a1489d3c2d44645260123e40e106ea89a4977

---
+++ dev/bench/awslogs/ubuntu/data.js
@@ -0,0 +1,50 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1696025974943,
+  "repoUrl": "https://github.com/aws/shim-loggers-for-containerd",
+  "entries": {
+    "Benchmark for awslogs": [
+      {
+        "commit": {
+          "author": {
+            "email": "ningziwe@amazon.com",
+            "name": "Ziwen Ning",
+            "username": "ningziwen"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d03a1489d3c2d44645260123e40e106ea89a4977",
+          "message": "ci: add basic benchmarking (#82)\n\n*Description of changes:*\r\n\r\nadd benchmarking for time and memory of sending a 1MB log\r\n\r\nResult page:\r\nhttps://aws.github.io/shim-loggers-for-containerd/dev/bench/\r\n\r\nUX improvements will come later in the gh-pages branch.\r\n\r\n\r\nBy submitting this pull request, I confirm that you can use, modify,\r\ncopy, and redistribute this contribution, under the terms of your\r\nchoice.\r\n\r\nSigned-off-by: Ziwen Ning <ningziwe@amazon.com>",
+          "timestamp": "2023-09-29T15:15:33-07:00",
+          "tree_id": "b2ed61664e0b68613dc0ae063baafe02520c112b",
+          "url": "https://github.com/aws/shim-loggers-for-containerd/commit/d03a1489d3c2d44645260123e40e106ea89a4977"
+        },
+        "date": 1696025973800,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkAwslogs - ns/op",
+            "value": 8503219549,
+            "unit": "ns/op",
+            "extra": "1 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkAwslogs - B/op",
+            "value": 43238336,
+            "unit": "B/op",
+            "extra": "1 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkAwslogs - allocs/op",
+            "value": 74658,
+            "unit": "allocs/op",
+            "extra": "1 times\n2 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
