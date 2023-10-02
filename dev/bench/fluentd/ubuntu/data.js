718f0f6129b0417f2f78b87e251dcb5c648faaa4
github-action-benchmark <github@users.noreply.github.com>
2023-10-02 18:01:13 +0000

    add Benchmark for awslogs (go) benchmark result for 0fe081af7810db59dd851915ee82e9604e725b34

--- dev/bench/fluentd/ubuntu/data.js
+++ dev/bench/fluentd/ubuntu/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1696025833936,
+  "lastUpdate": 1696269673844,
   "repoUrl": "https://github.com/aws/shim-loggers-for-containerd",
   "entries": {
     "Benchmark for awslogs": [
@@ -40,6 +40,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkFluentd - allocs/op",
             "value": 73526,
+            "unit": "allocs/op",
+            "extra": "1 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ningziwe@amazon.com",
+            "name": "Ziwen Ning",
+            "username": "ningziwen"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0fe081af7810db59dd851915ee82e9604e725b34",
+          "message": "ci: add lint for e2e and unit (#84)\n\n*Issue #, if available:*\r\n\r\n*Description of changes:*\r\n\r\n\r\nBy submitting this pull request, I confirm that you can use, modify,\r\ncopy, and redistribute this contribution, under the terms of your\r\nchoice.\r\n\r\nSigned-off-by: Ziwen Ning <ningziwe@amazon.com>",
+          "timestamp": "2023-10-02T10:59:37-07:00",
+          "tree_id": "0ea6ef376743fc77631ee2a4d138b5ef57a2d3f2",
+          "url": "https://github.com/aws/shim-loggers-for-containerd/commit/0fe081af7810db59dd851915ee82e9604e725b34"
+        },
+        "date": 1696269673317,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkFluentd - ns/op",
+            "value": 8880350431,
+            "unit": "ns/op",
+            "extra": "1 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkFluentd - B/op",
+            "value": 43102688,
+            "unit": "B/op",
+            "extra": "1 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkFluentd - allocs/op",
+            "value": 73491,
             "unit": "allocs/op",
             "extra": "1 times\n2 procs"
           }
