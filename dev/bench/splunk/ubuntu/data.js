ffb3ee83cab5d5ecd1ea0c9af7f4f59de8899766
github-action-benchmark <github@users.noreply.github.com>
2024-03-21 15:38:37 +0000

    add Benchmark for splunk (go) benchmark result for d4df5dd67316689b02ca909baa796a5ba7dac30d

--- dev/bench/splunk/ubuntu/data.js
+++ dev/bench/splunk/ubuntu/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1711034867976,
+  "lastUpdate": 1711035517904,
   "repoUrl": "https://github.com/aws/shim-loggers-for-containerd",
   "entries": {
     "Benchmark for splunk": [
@@ -262,6 +262,54 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkSplunk - allocs/op",
             "value": 65609,
+            "unit": "allocs/op",
+            "extra": "1 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d4df5dd67316689b02ca909baa796a5ba7dac30d",
+          "message": "chore(deps): bump github.com/containerd/containerd from 1.6.18 to 1.6.26 (#87)\n\nBumps\r\n[github.com/containerd/containerd](https://github.com/containerd/containerd)\r\nfrom 1.6.18 to 1.6.26.\r\n<details>\r\n<summary>Release notes</summary>\r\n<p><em>Sourced from <a\r\nhref=\"https://github.com/containerd/containerd/releases\">github.com/containerd/containerd's\r\nreleases</a>.</em></p>\r\n<blockquote>\r\n<h2>containerd 1.6.26</h2>\r\n<p>Welcome to the v1.6.26 release of containerd!</p>\r\n<p>The twenty-sixth patch release for containerd 1.6 contains various\r\nfixes and updates.</p>\r\n<h3>Notable Updates</h3>\r\n<ul>\r\n<li><strong>Fix windows default path overwrite issue</strong> (<a\r\nhref=\"https://redirect.github.com/containerd/containerd/pull/9441\">#9441</a>)</li>\r\n<li><strong>Update push to inherit distribution sources from\r\nparent</strong> (<a\r\nhref=\"https://redirect.github.com/containerd/containerd/pull/9453\">#9453</a>)</li>\r\n<li><strong>Mask <code>/sys/devices/virtual/powercap</code> path in\r\nruntime spec and deny in default apparmor profile</strong> (<a\r\nhref=\"https://github.com/containerd/containerd/security/advisories/GHSA-7ww5-4wqc-m92c\">GHSA-7ww5-4wqc-m92c</a>)</li>\r\n</ul>\r\n<h3>Deprecation Warnings</h3>\r\n<ul>\r\n<li><strong>Emit deprecation warning for AUFS snapshotter usage</strong>\r\n(<a\r\nhref=\"https://redirect.github.com/containerd/containerd/pull/9448\">#9448</a>)</li>\r\n<li><strong>Emit deprecation warning for v1 runtime usage</strong> (<a\r\nhref=\"https://redirect.github.com/containerd/containerd/pull/9468\">#9468</a>)</li>\r\n<li><strong>Emit deprecation warning for CRI v1alpha1 usage</strong> (<a\r\nhref=\"https://redirect.github.com/containerd/containerd/pull/9468\">#9468</a>)</li>\r\n</ul>\r\n<p>See the changelog for complete list of changes</p>\r\n<p>Please try out the release binaries and report any issues at\r\n<a\r\nhref=\"https://github.com/containerd/containerd/issues\">https://github.com/containerd/containerd/issues</a>.</p>\r\n<h3>Contributors</h3>\r\n<ul>\r\n<li>Samuel Karp</li>\r\n<li>Derek McGowan</li>\r\n<li>Kohei Tokunaga</li>\r\n<li>Phil Estes</li>\r\n<li>Bjorn Neergaard</li>\r\n<li>Sebastiaan van Stijn</li>\r\n<li>Brian Goff</li>\r\n<li>Charity Kathure</li>\r\n<li>Kazuyoshi Kato</li>\r\n<li>Milas Bowman</li>\r\n<li>Wei Fu</li>\r\n<li>ruiwen-zhao</li>\r\n</ul>\r\n<h3>Changes</h3>\r\n<!-- raw HTML omitted -->\r\n<ul>\r\n<li>[release/1.6] Prepare release notes for v1.6.26 (<a\r\nhref=\"https://redirect.github.com/containerd/containerd/pull/9490\">#9490</a>)\r\n<ul>\r\n<li><a\r\nhref=\"https://github.com/containerd/containerd/commit/ac5c5d3e03ab3c5b8103a1c0bd9931389f7a8fcf\"><code>ac5c5d3e0</code></a>\r\nPrepare release notes for v1.6.26</li>\r\n</ul>\r\n</li>\r\n<li>Github Security Advisory <a\r\nhref=\"https://github.com/containerd/containerd/security/advisories/GHSA-7ww5-4wqc-m92c\">GHSA-7ww5-4wqc-m92c</a>\r\n<ul>\r\n<li><a\r\nhref=\"https://github.com/containerd/containerd/commit/02f07fe1994a3ddda3626c1ede2e32bc82b8e426\"><code>02f07fe19</code></a>\r\ncontrib/apparmor: deny /sys/devices/virtual/powercap</li>\r\n<li><a\r\nhref=\"https://github.com/containerd/containerd/commit/c94577e78d2924ddeb90d1601e31b50ee3acac48\"><code>c94577e78</code></a>\r\noci/spec: deny /sys/devices/virtual/powercap</li>\r\n</ul>\r\n</li>\r\n<li>[release/1.6] update to go1.20.12, test go1.21.5 (<a\r\nhref=\"https://redirect.github.com/containerd/containerd/pull/9472\">#9472</a>)\r\n<ul>\r\n<li><a\r\nhref=\"https://github.com/containerd/containerd/commit/7cbdfc92ef38f789f1a2773fa6fac405d361a6cc\"><code>7cbdfc92e</code></a>\r\nupdate to go1.20.12, test go1.21.5</li>\r\n<li><a\r\nhref=\"https://github.com/containerd/containerd/commit/024b1cce6b27f10e00bb9bde33a5fe9563545f8d\"><code>024b1cce6</code></a>\r\nupdate to go1.20.11, test go1.21.4</li>\r\n</ul>\r\n</li>\r\n<li>[release/1.6] Add cri-api v1alpha2 usage warning to all api calls\r\n(<a\r\nhref=\"https://redirect.github.com/containerd/containerd/pull/9484\">#9484</a>)</li>\r\n</ul>\r\n<!-- raw HTML omitted -->\r\n</blockquote>\r\n<p>... (truncated)</p>\r\n</details>\r\n<details>\r\n<summary>Commits</summary>\r\n<ul>\r\n<li><a\r\nhref=\"https://github.com/containerd/containerd/commit/3dd1e886e55dd695541fdcd67420c2888645a495\"><code>3dd1e88</code></a>\r\nMerge pull request <a\r\nhref=\"https://redirect.github.com/containerd/containerd/issues/9490\">#9490</a>\r\nfrom dmcgowan/prepare-1.6.26</li>\r\n<li><a\r\nhref=\"https://github.com/containerd/containerd/commit/746b910f05855c8bfdb4415a1c0f958b234910e5\"><code>746b910</code></a>\r\nMerge pull request from GHSA-7ww5-4wqc-m92c</li>\r\n<li><a\r\nhref=\"https://github.com/containerd/containerd/commit/ac5c5d3e03ab3c5b8103a1c0bd9931389f7a8fcf\"><code>ac5c5d3</code></a>\r\nPrepare release notes for v1.6.26</li>\r\n<li><a\r\nhref=\"https://github.com/containerd/containerd/commit/e7ca005043f6974536c3f8e0da42f93b5bdc2879\"><code>e7ca005</code></a>\r\nMerge pull request <a\r\nhref=\"https://redirect.github.com/containerd/containerd/issues/9472\">#9472</a>\r\nfrom thaJeztah/1.6_update_golang_1.20.12</li>\r\n<li><a\r\nhref=\"https://github.com/containerd/containerd/commit/7cbdfc92ef38f789f1a2773fa6fac405d361a6cc\"><code>7cbdfc9</code></a>\r\nupdate to go1.20.12, test go1.21.5</li>\r\n<li><a\r\nhref=\"https://github.com/containerd/containerd/commit/024b1cce6b27f10e00bb9bde33a5fe9563545f8d\"><code>024b1cc</code></a>\r\nupdate to go1.20.11, test go1.21.4</li>\r\n<li><a\r\nhref=\"https://github.com/containerd/containerd/commit/2e404598e7da93f4ad8b13bb6119441a5e3c83b0\"><code>2e40459</code></a>\r\nMerge pull request <a\r\nhref=\"https://redirect.github.com/containerd/containerd/issues/9484\">#9484</a>\r\nfrom ruiwen-zhao/cri-api-warning-1.6</li>\r\n<li><a\r\nhref=\"https://github.com/containerd/containerd/commit/64e56bfde95828660971673d20952f275cc2c0ba\"><code>64e56bf</code></a>\r\nAdd cri-api v1alpha2 usage warning to all api calls</li>\r\n<li><a\r\nhref=\"https://github.com/containerd/containerd/commit/c566b7d46668de23d2881eb86ce1b76ca23c8a75\"><code>c566b7d</code></a>\r\nMerge pull request <a\r\nhref=\"https://redirect.github.com/containerd/containerd/issues/9468\">#9468</a>\r\nfrom samuelkarp/deprecation-warning-runtime-1.6</li>\r\n<li><a\r\nhref=\"https://github.com/containerd/containerd/commit/efefd3bf334b5df0e97bff0be61ba906a9b3b528\"><code>efefd3b</code></a>\r\ntasks: emit warning for runc v1 runtime</li>\r\n<li>Additional commits viewable in <a\r\nhref=\"https://github.com/containerd/containerd/compare/v1.6.18...v1.6.26\">compare\r\nview</a></li>\r\n</ul>\r\n</details>\r\n<br />\r\n\r\n\r\n[![Dependabot compatibility\r\nscore](https://dependabot-badges.githubapp.com/badges/compatibility_score?dependency-name=github.com/containerd/containerd&package-manager=go_modules&previous-version=1.6.18&new-version=1.6.26)](https://docs.github.com/en/github/managing-security-vulnerabilities/about-dependabot-security-updates#about-compatibility-scores)\r\n\r\nYou can trigger a rebase of this PR by commenting `@dependabot rebase`.\r\n\r\n[//]: # (dependabot-automerge-start)\r\n[//]: # (dependabot-automerge-end)\r\n\r\n---\r\n\r\n<details>\r\n<summary>Dependabot commands and options</summary>\r\n<br />\r\n\r\nYou can trigger Dependabot actions by commenting on this PR:\r\n- `@dependabot rebase` will rebase this PR\r\n- `@dependabot recreate` will recreate this PR, overwriting any edits\r\nthat have been made to it\r\n- `@dependabot merge` will merge this PR after your CI passes on it\r\n- `@dependabot squash and merge` will squash and merge this PR after\r\nyour CI passes on it\r\n- `@dependabot cancel merge` will cancel a previously requested merge\r\nand block automerging\r\n- `@dependabot reopen` will reopen this PR if it is closed\r\n- `@dependabot close` will close this PR and stop Dependabot recreating\r\nit. You can achieve the same result by closing it manually\r\n- `@dependabot show <dependency name> ignore conditions` will show all\r\nof the ignore conditions of the specified dependency\r\n- `@dependabot ignore this major version` will close this PR and stop\r\nDependabot creating any more for this major version (unless you reopen\r\nthe PR or upgrade to it yourself)\r\n- `@dependabot ignore this minor version` will close this PR and stop\r\nDependabot creating any more for this minor version (unless you reopen\r\nthe PR or upgrade to it yourself)\r\n- `@dependabot ignore this dependency` will close this PR and stop\r\nDependabot creating any more for this dependency (unless you reopen the\r\nPR or upgrade to it yourself)\r\nYou can disable automated security fix PRs for this repo from the\r\n[Security Alerts\r\npage](https://github.com/aws/shim-loggers-for-containerd/network/alerts).\r\n\r\n</details>\r\n\r\n> **Note**\r\n> Automatic rebases have been disabled on this pull request as it has\r\nbeen open for over 30 days.\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2024-03-21T08:35:38-07:00",
+          "tree_id": "6fbea479e21f5df4ff2b8f4ea39099c741b37d7a",
+          "url": "https://github.com/aws/shim-loggers-for-containerd/commit/d4df5dd67316689b02ca909baa796a5ba7dac30d"
+        },
+        "date": 1711035517496,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkSplunk",
+            "value": 7399885744,
+            "unit": "ns/op\t41915688 B/op\t   66010 allocs/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSplunk - ns/op",
+            "value": 7399885744,
+            "unit": "ns/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSplunk - B/op",
+            "value": 41915688,
+            "unit": "B/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSplunk - allocs/op",
+            "value": 66010,
             "unit": "allocs/op",
             "extra": "1 times\n4 procs"
           }
