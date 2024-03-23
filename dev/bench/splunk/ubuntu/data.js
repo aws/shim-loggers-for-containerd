4c936f48c72c0964fbd82162d91456174b7e7534
github-action-benchmark <github@users.noreply.github.com>
2024-03-23 05:43:11 +0000

    add Benchmark for splunk (go) benchmark result for b2117287609243c3254d72c7ae0711f546a22e59

--- dev/bench/splunk/ubuntu/data.js
+++ dev/bench/splunk/ubuntu/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1711172495363,
+  "lastUpdate": 1711172591787,
   "repoUrl": "https://github.com/aws/shim-loggers-for-containerd",
   "entries": {
     "Benchmark for splunk": [
@@ -406,6 +406,54 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkSplunk - allocs/op",
             "value": 66101,
+            "unit": "allocs/op",
+            "extra": "1 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "55906459+austinvazquez@users.noreply.github.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "distinct": true,
+          "id": "b2117287609243c3254d72c7ae0711f546a22e59",
+          "message": "chore(deps): bump golangci/golangci-lint-action from 3 to 4\n\nBumps [golangci/golangci-lint-action](https://github.com/golangci/golangci-lint-action) from 3 to 4.\n- [Release notes](https://github.com/golangci/golangci-lint-action/releases)\n- [Commits](https://github.com/golangci/golangci-lint-action/compare/v3...v4)\n\n---\nupdated-dependencies:\n- dependency-name: golangci/golangci-lint-action\n  dependency-type: direct:production\n  update-type: version-update:semver-major\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2024-03-22T22:40:20-07:00",
+          "tree_id": "57136a13e94f53e0cca574b28a4601e91875affc",
+          "url": "https://github.com/aws/shim-loggers-for-containerd/commit/b2117287609243c3254d72c7ae0711f546a22e59"
+        },
+        "date": 1711172591322,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkSplunk",
+            "value": 8455131754,
+            "unit": "ns/op\t40734656 B/op\t   65810 allocs/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSplunk - ns/op",
+            "value": 8455131754,
+            "unit": "ns/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSplunk - B/op",
+            "value": 40734656,
+            "unit": "B/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSplunk - allocs/op",
+            "value": 65810,
             "unit": "allocs/op",
             "extra": "1 times\n4 procs"
           }
