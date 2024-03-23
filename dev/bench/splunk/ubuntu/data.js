18a5f9adb6c2c8378b3ec35c2706939fc390ddc9
github-action-benchmark <github@users.noreply.github.com>
2024-03-23 05:50:05 +0000

    add Benchmark for splunk (go) benchmark result for 8d6879748f8c6dd3c0baccdd200afaa815c6283e

--- dev/bench/splunk/ubuntu/data.js
+++ dev/bench/splunk/ubuntu/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1711172782228,
+  "lastUpdate": 1711173004999,
   "repoUrl": "https://github.com/aws/shim-loggers-for-containerd",
   "entries": {
     "Benchmark for splunk": [
@@ -598,6 +598,54 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkSplunk - allocs/op",
             "value": 66215,
+            "unit": "allocs/op",
+            "extra": "1 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "55906459+austinvazquez@users.noreply.github.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "distinct": true,
+          "id": "8d6879748f8c6dd3c0baccdd200afaa815c6283e",
+          "message": "chore(deps): bump github.com/onsi/gomega from 1.27.8 to 1.32.0\n\nBumps [github.com/onsi/gomega](https://github.com/onsi/gomega) from 1.27.8 to 1.32.0.\n- [Release notes](https://github.com/onsi/gomega/releases)\n- [Changelog](https://github.com/onsi/gomega/blob/master/CHANGELOG.md)\n- [Commits](https://github.com/onsi/gomega/compare/v1.27.8...v1.32.0)\n\n---\nupdated-dependencies:\n- dependency-name: github.com/onsi/gomega\n  dependency-type: direct:production\n  update-type: version-update:semver-minor\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2024-03-22T22:47:11-07:00",
+          "tree_id": "73e861aa5067bbb8e0abf162a901d0dc38b35e2f",
+          "url": "https://github.com/aws/shim-loggers-for-containerd/commit/8d6879748f8c6dd3c0baccdd200afaa815c6283e"
+        },
+        "date": 1711173004434,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkSplunk",
+            "value": 8203830019,
+            "unit": "ns/op\t40522368 B/op\t   65831 allocs/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSplunk - ns/op",
+            "value": 8203830019,
+            "unit": "ns/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSplunk - B/op",
+            "value": 40522368,
+            "unit": "B/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSplunk - allocs/op",
+            "value": 65831,
             "unit": "allocs/op",
             "extra": "1 times\n4 procs"
           }
