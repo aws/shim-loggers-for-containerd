c10fce48e55c1dba70dc2e32e065d22e81eefaa0
github-action-benchmark <github@users.noreply.github.com>
2024-03-25 18:30:53 +0000

    add Benchmark for awslogs (go) benchmark result for 32d47fd081d635894ad04b61a26686e56f7a049a

--- dev/bench/fluentd/ubuntu/data.js
+++ dev/bench/fluentd/ubuntu/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1711381499299,
+  "lastUpdate": 1711391453955,
   "repoUrl": "https://github.com/aws/shim-loggers-for-containerd",
   "entries": {
     "Benchmark for awslogs": [
@@ -934,6 +934,54 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkFluentd - allocs/op",
             "value": 67403,
+            "unit": "allocs/op",
+            "extra": "1 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "55906459+austinvazquez@users.noreply.github.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "distinct": true,
+          "id": "32d47fd081d635894ad04b61a26686e56f7a049a",
+          "message": "chore(deps): bump github.com/containerd/containerd from 1.6.26 to 1.7.14\n\nBumps [github.com/containerd/containerd](https://github.com/containerd/containerd) from 1.6.26 to 1.7.14.\n- [Release notes](https://github.com/containerd/containerd/releases)\n- [Changelog](https://github.com/containerd/containerd/blob/main/RELEASES.md)\n- [Commits](https://github.com/containerd/containerd/compare/v1.6.26...v1.7.14)\n\n---\nupdated-dependencies:\n- dependency-name: github.com/containerd/containerd\n  dependency-type: direct:production\n  update-type: version-update:semver-minor\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2024-03-25T11:29:35-07:00",
+          "tree_id": "a50376464b94269358c6cc773273b44d07e40eb1",
+          "url": "https://github.com/aws/shim-loggers-for-containerd/commit/32d47fd081d635894ad04b61a26686e56f7a049a"
+        },
+        "date": 1711391453350,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkFluentd",
+            "value": 8604298605,
+            "unit": "ns/op\t38924448 B/op\t   66898 allocs/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFluentd - ns/op",
+            "value": 8604298605,
+            "unit": "ns/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFluentd - B/op",
+            "value": 38924448,
+            "unit": "B/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFluentd - allocs/op",
+            "value": 66898,
             "unit": "allocs/op",
             "extra": "1 times\n4 procs"
           }
