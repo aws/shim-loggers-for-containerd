f6d0d2b2806c8e62968c86c94b7342a707732750
github-action-benchmark <github@users.noreply.github.com>
2024-03-27 23:25:15 +0000

    add Benchmark for awslogs (go) benchmark result for 36f3d4a305d340c489e8df3c010338d23a360ed3

--- dev/bench/fluentd/ubuntu/data.js
+++ dev/bench/fluentd/ubuntu/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1711581902058,
+  "lastUpdate": 1711581915206,
   "repoUrl": "https://github.com/aws/shim-loggers-for-containerd",
   "entries": {
     "Benchmark for awslogs": [
@@ -1078,6 +1078,54 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkFluentd - allocs/op",
             "value": 66940,
+            "unit": "allocs/op",
+            "extra": "1 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "macedonv@amazon.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "committer": {
+            "email": "55906459+austinvazquez@users.noreply.github.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "distinct": true,
+          "id": "36f3d4a305d340c489e8df3c010338d23a360ed3",
+          "message": "chore: add codeowners\n\nSigned-off-by: Austin Vazquez <macedonv@amazon.com>",
+          "timestamp": "2024-03-27T16:24:06-07:00",
+          "tree_id": "f2c6cf40203a950f22bd848a64f32cbb799772a5",
+          "url": "https://github.com/aws/shim-loggers-for-containerd/commit/36f3d4a305d340c489e8df3c010338d23a360ed3"
+        },
+        "date": 1711581914781,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkFluentd",
+            "value": 7808941217,
+            "unit": "ns/op\t39731152 B/op\t   66856 allocs/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFluentd - ns/op",
+            "value": 7808941217,
+            "unit": "ns/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFluentd - B/op",
+            "value": 39731152,
+            "unit": "B/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFluentd - allocs/op",
+            "value": 66856,
             "unit": "allocs/op",
             "extra": "1 times\n4 procs"
           }
