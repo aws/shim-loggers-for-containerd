10121c5cd8f2029f10eb022cf59c22f272faedca
github-action-benchmark <github@users.noreply.github.com>
2024-04-01 04:42:53 +0000

    add Benchmark for splunk (go) benchmark result for a072fe5738b712e86e606ea06c098bdf474519c8

--- dev/bench/splunk/ubuntu/data.js
+++ dev/bench/splunk/ubuntu/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1711582718205,
+  "lastUpdate": 1711946573503,
   "repoUrl": "https://github.com/aws/shim-loggers-for-containerd",
   "entries": {
     "Benchmark for splunk": [
@@ -1126,6 +1126,54 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkSplunk - allocs/op",
             "value": 68353,
+            "unit": "allocs/op",
+            "extra": "1 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "55906459+austinvazquez@users.noreply.github.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "distinct": true,
+          "id": "a072fe5738b712e86e606ea06c098bdf474519c8",
+          "message": "chore(deps): bump github.com/aws/smithy-go from 1.20.1 to 1.20.2\n\nBumps [github.com/aws/smithy-go](https://github.com/aws/smithy-go) from 1.20.1 to 1.20.2.\n- [Release notes](https://github.com/aws/smithy-go/releases)\n- [Changelog](https://github.com/aws/smithy-go/blob/main/CHANGELOG.md)\n- [Commits](https://github.com/aws/smithy-go/compare/v1.20.1...v1.20.2)\n\n---\nupdated-dependencies:\n- dependency-name: github.com/aws/smithy-go\n  dependency-type: direct:production\n  update-type: version-update:semver-patch\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2024-03-31T21:39:32-07:00",
+          "tree_id": "bdd0db13d41beca0be6ef0cd2c16a92672aaddab",
+          "url": "https://github.com/aws/shim-loggers-for-containerd/commit/a072fe5738b712e86e606ea06c098bdf474519c8"
+        },
+        "date": 1711946573068,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkSplunk",
+            "value": 7777365584,
+            "unit": "ns/op\t40748760 B/op\t   66966 allocs/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSplunk - ns/op",
+            "value": 7777365584,
+            "unit": "ns/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSplunk - B/op",
+            "value": 40748760,
+            "unit": "B/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSplunk - allocs/op",
+            "value": 66966,
             "unit": "allocs/op",
             "extra": "1 times\n4 procs"
           }
