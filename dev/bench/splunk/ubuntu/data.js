21da2365171a3ad7b560ce3e139b5eb204a35eea
github-action-benchmark <github@users.noreply.github.com>
2024-08-27 15:03:44 +0000

    add Benchmark for splunk (go) benchmark result for 7f0490366f6cdd1588f955329d12aad55796a9ac

--- dev/bench/splunk/ubuntu/data.js
+++ dev/bench/splunk/ubuntu/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1724769823329,
+  "lastUpdate": 1724771024623,
   "repoUrl": "https://github.com/aws/shim-loggers-for-containerd",
   "entries": {
     "Benchmark for splunk": [
@@ -2182,6 +2182,54 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkSplunk - allocs/op",
             "value": 67919,
+            "unit": "allocs/op",
+            "extra": "1 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "alvajus@amazon.com",
+            "name": "Justin Alvarez",
+            "username": "pendo324"
+          },
+          "committer": {
+            "email": "pendo324@users.noreply.github.com",
+            "name": "Justin",
+            "username": "pendo324"
+          },
+          "distinct": true,
+          "id": "7f0490366f6cdd1588f955329d12aad55796a9ac",
+          "message": "ci: migrate to EndpointResolverV2 since old methods are deprecated\n\nSigned-off-by: Justin Alvarez <alvajus@amazon.com>",
+          "timestamp": "2024-08-27T11:00:42-04:00",
+          "tree_id": "f34e77857dbd0bf4be982ecc8077c739c4b846db",
+          "url": "https://github.com/aws/shim-loggers-for-containerd/commit/7f0490366f6cdd1588f955329d12aad55796a9ac"
+        },
+        "date": 1724771024357,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkSplunk",
+            "value": 7381896684,
+            "unit": "ns/op\t43693528 B/op\t   67780 allocs/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSplunk - ns/op",
+            "value": 7381896684,
+            "unit": "ns/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSplunk - B/op",
+            "value": 43693528,
+            "unit": "B/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSplunk - allocs/op",
+            "value": 67780,
             "unit": "allocs/op",
             "extra": "1 times\n4 procs"
           }
