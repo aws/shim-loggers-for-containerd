6dd65c3d48368a9356a6fc8f25ff8586af56b7a9
github-action-benchmark <github@users.noreply.github.com>
2025-03-12 23:40:35 +0000

    add Benchmark for awslogs (go) benchmark result for 2b0c9cb68be6f1053ec5c5bab1e6b37dc4f4deda

--- dev/bench/awslogs/ubuntu/data.js
+++ dev/bench/awslogs/ubuntu/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1741821049930,
+  "lastUpdate": 1741822835553,
   "repoUrl": "https://github.com/aws/shim-loggers-for-containerd",
   "entries": {
     "Benchmark for awslogs": [
@@ -3094,6 +3094,54 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkAwslogs - allocs/op",
             "value": 69909,
+            "unit": "allocs/op",
+            "extra": "1 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "55906459+austinvazquez@users.noreply.github.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "distinct": true,
+          "id": "2b0c9cb68be6f1053ec5c5bab1e6b37dc4f4deda",
+          "message": "chore(deps): bump github.com/onsi/ginkgo/v2 from 2.19.0 to 2.23.0\n\nBumps [github.com/onsi/ginkgo/v2](https://github.com/onsi/ginkgo) from 2.19.0 to 2.23.0.\n- [Release notes](https://github.com/onsi/ginkgo/releases)\n- [Changelog](https://github.com/onsi/ginkgo/blob/master/CHANGELOG.md)\n- [Commits](https://github.com/onsi/ginkgo/compare/v2.19.0...v2.23.0)\n\n---\nupdated-dependencies:\n- dependency-name: github.com/onsi/ginkgo/v2\n  dependency-type: direct:production\n  update-type: version-update:semver-minor\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2025-03-12T18:37:51-05:00",
+          "tree_id": "8a838b2a68409341cd2110bd1495d90a71366fc7",
+          "url": "https://github.com/aws/shim-loggers-for-containerd/commit/2b0c9cb68be6f1053ec5c5bab1e6b37dc4f4deda"
+        },
+        "date": 1741822835056,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkAwslogs",
+            "value": 9090150639,
+            "unit": "ns/op\t42633384 B/op\t   70825 allocs/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkAwslogs - ns/op",
+            "value": 9090150639,
+            "unit": "ns/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkAwslogs - B/op",
+            "value": 42633384,
+            "unit": "B/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkAwslogs - allocs/op",
+            "value": 70825,
             "unit": "allocs/op",
             "extra": "1 times\n4 procs"
           }
