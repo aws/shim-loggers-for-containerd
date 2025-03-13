f6a8c681dc7f9d2e434629c5b074af9e8ceb1581
github-action-benchmark <github@users.noreply.github.com>
2025-03-13 04:19:56 +0000

    add Benchmark for splunk (go) benchmark result for 346a924db328f540389a6e51d3d303c2318fe8a5

--- dev/bench/splunk/ubuntu/data.js
+++ dev/bench/splunk/ubuntu/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1741839564021,
+  "lastUpdate": 1741839595993,
   "repoUrl": "https://github.com/aws/shim-loggers-for-containerd",
   "entries": {
     "Benchmark for splunk": [
@@ -3334,6 +3334,54 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkSplunk - allocs/op",
             "value": 69870,
+            "unit": "allocs/op",
+            "extra": "1 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "macedonv@amazon.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "committer": {
+            "email": "55906459+austinvazquez@users.noreply.github.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "distinct": true,
+          "id": "346a924db328f540389a6e51d3d303c2318fe8a5",
+          "message": "ci: add git secrets scanning\n\nThis change adds a GitHub Actions job to validate git secrets are not\nsubmitted to version control.\n\nSigned-off-by: Austin Vazquez <macedonv@amazon.com>",
+          "timestamp": "2025-03-12T23:16:22-05:00",
+          "tree_id": "9d53f2505127f9c3dd10a9007e620429cb446461",
+          "url": "https://github.com/aws/shim-loggers-for-containerd/commit/346a924db328f540389a6e51d3d303c2318fe8a5"
+        },
+        "date": 1741839595529,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkSplunk",
+            "value": 8001858098,
+            "unit": "ns/op\t42531592 B/op\t   69713 allocs/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSplunk - ns/op",
+            "value": 8001858098,
+            "unit": "ns/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSplunk - B/op",
+            "value": 42531592,
+            "unit": "B/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSplunk - allocs/op",
+            "value": 69713,
             "unit": "allocs/op",
             "extra": "1 times\n4 procs"
           }
