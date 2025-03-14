8e31695609c778b1103a68da07f4333553f7b59d
github-action-benchmark <github@users.noreply.github.com>
2025-03-14 20:03:53 +0000

    add Benchmark for awslogs (go) benchmark result for 32de70d0dbfb6388a10762f0885f66bb2502f664

--- dev/bench/awslogs/ubuntu/data.js
+++ dev/bench/awslogs/ubuntu/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1741839553138,
+  "lastUpdate": 1741982633879,
   "repoUrl": "https://github.com/aws/shim-loggers-for-containerd",
   "entries": {
     "Benchmark for awslogs": [
@@ -3382,6 +3382,54 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkAwslogs - allocs/op",
             "value": 71097,
+            "unit": "allocs/op",
+            "extra": "1 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "55906459+austinvazquez@users.noreply.github.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "distinct": true,
+          "id": "32de70d0dbfb6388a10762f0885f66bb2502f664",
+          "message": "chore(deps): bump github.com/aws/smithy-go from 1.22.2 to 1.22.3\n\nBumps [github.com/aws/smithy-go](https://github.com/aws/smithy-go) from 1.22.2 to 1.22.3.\n- [Release notes](https://github.com/aws/smithy-go/releases)\n- [Changelog](https://github.com/aws/smithy-go/blob/main/CHANGELOG.md)\n- [Commits](https://github.com/aws/smithy-go/compare/v1.22.2...v1.22.3)\n\n---\nupdated-dependencies:\n- dependency-name: github.com/aws/smithy-go\n  dependency-type: direct:production\n  update-type: version-update:semver-patch\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2025-03-14T15:01:05-05:00",
+          "tree_id": "0a29531a1b08d00a0bff21920474d6b2306fcfa4",
+          "url": "https://github.com/aws/shim-loggers-for-containerd/commit/32de70d0dbfb6388a10762f0885f66bb2502f664"
+        },
+        "date": 1741982633354,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkAwslogs",
+            "value": 9345032655,
+            "unit": "ns/op\t40562072 B/op\t   71049 allocs/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkAwslogs - ns/op",
+            "value": 9345032655,
+            "unit": "ns/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkAwslogs - B/op",
+            "value": 40562072,
+            "unit": "B/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkAwslogs - allocs/op",
+            "value": 71049,
             "unit": "allocs/op",
             "extra": "1 times\n4 procs"
           }
