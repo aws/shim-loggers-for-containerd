410604ff7a40c847bc5b66b9f9fe8c92350608a4
github-action-benchmark <github@users.noreply.github.com>
2025-06-23 22:57:31 +0000

    add Benchmark for awslogs (go) benchmark result for 6ba0f849bba786cedcb150881fdebfefed7cfb74

--- dev/bench/fluentd/ubuntu/data.js
+++ dev/bench/fluentd/ubuntu/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1747275050579,
+  "lastUpdate": 1750719451435,
   "repoUrl": "https://github.com/aws/shim-loggers-for-containerd",
   "entries": {
     "Benchmark for awslogs": [
@@ -4246,6 +4246,54 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkFluentd - allocs/op",
             "value": 70046,
+            "unit": "allocs/op",
+            "extra": "1 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "aithal@amazon.com",
+            "name": "Anirudh Aithal",
+            "username": "aaithal"
+          },
+          "committer": {
+            "email": "aithal@amazon.com",
+            "name": "Anirudh Aithal",
+            "username": "aaithal"
+          },
+          "distinct": true,
+          "id": "6ba0f849bba786cedcb150881fdebfefed7cfb74",
+          "message": "fluentd: set default timeout for writes\n\nSet a default value for fluentd write timeout. This helps the client\nexit the write operation with an error if the server is unresponsive.\n\nSigned-off-by: Anirudh Aithal <aithal@amazon.com>",
+          "timestamp": "2025-06-23T15:56:14-07:00",
+          "tree_id": "2bccb3374358a8580c9de9d1e010b3bbc0d3d9b0",
+          "url": "https://github.com/aws/shim-loggers-for-containerd/commit/6ba0f849bba786cedcb150881fdebfefed7cfb74"
+        },
+        "date": 1750719451069,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkFluentd",
+            "value": 8299012939,
+            "unit": "ns/op\t27002736 B/op\t   69933 allocs/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFluentd - ns/op",
+            "value": 8299012939,
+            "unit": "ns/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFluentd - B/op",
+            "value": 27002736,
+            "unit": "B/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFluentd - allocs/op",
+            "value": 69933,
             "unit": "allocs/op",
             "extra": "1 times\n4 procs"
           }
